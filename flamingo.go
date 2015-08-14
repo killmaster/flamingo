151d8e9a2bfa99c1bf5283f4be2f8f4960ab33ee
Carlos Martins <carlosmartins8@gmail.com>
2015-08-14 22:22:37 +0100

    Flamingo is flying!

--- flamingo.go
+++ flamingo.go
@@ -25,6 +25,14 @@ type Configuration struct {
 	ApiSecret string
 }
 
+var nick string
+var user string
+var server string
+var channel string
+var api *lastfm.Api
+var db *sql.DB
+var irccon *irc.Connection = irc.IRC(nick, user)
+
 func main() {
 	file, _ := os.Open("config.json")
 	decoder := json.NewDecoder(file)
@@ -33,23 +41,28 @@ func main() {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	fmt.Println(configuration.Nick) // output: [UserA, UserB]
 
-	nick := configuration.Nick
-	user := configuration.User
-	server := configuration.Server
-	channel := configuration.Channel
+	nick = configuration.Nick
+	user = configuration.User
+	server = configuration.Server
+	channel = configuration.Channel
 
 	fmt.Println("Server: " + server)
+	fmt.Println("Nick: " + nick)
+	fmt.Println("User: " + user)
+	fmt.Println("channel: " + channel)
+	fmt.Println("API Key: " + configuration.ApiKey)
+	fmt.Println("API Secret: " + configuration.ApiKey)
 
-	api := lastfm.New(configuration.ApiKey, configuration.ApiSecret)
-	db, err := sql.Open("sqlite3", "./flamingo.db")
+	api = lastfm.New(configuration.ApiKey, configuration.ApiSecret)
+
+	db, err = sql.Open("sqlite3", "./flamingo.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	irccon := irc.IRC(nick, user)
+	irccon = irc.IRC(nick, user)
 	//irccon.Debug = true
 	//irccon.VerboseCallbackHandler = true
 	err = irccon.Connect(server)
@@ -66,73 +79,6 @@ func main() {
 		irccon.Join(channel)
 	})
 
-	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
-		req := strings.Split(e.Message(), " ")
-		if req[0] == "-np" {
-			var reply string
-			if len(req) > 1 {
-				result, err := api.User.GetRecentTracks(lastfm.P{"user": req[1]})
-				if err != nil {
-					reply = "Não quero"
-				} else if result.Tracks != nil {
-					if result.Tracks[0].NowPlaying == "true" {
-						reply = req[1] + " está agora a ouvir " +
-							result.Tracks[0].Artist.Name +
-							" - " + result.Tracks[0].Name
-					} else {
-						reply = req[1] + " ouviu " + result.Tracks[0].Artist.Name +
-							" - " + result.Tracks[0].Name
-					}
-				}
-				reply = "Não quero fdp, deslarga-me"
-			} else {
-				rows, err := db.Query("select lastfm from users where nick = '" + e.Nick + "'")
-				var lfm string
-				rows.Next()
-				rows.Scan(&lfm)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if lfm == "" {
-					reply = "Não sei qual é o teu username"
-				} else {
-					result, err := api.User.GetRecentTracks(lastfm.P{"user": lfm})
-					if err != nil {
-						reply = "Não quero"
-					} else {
-						if result.Tracks[0].NowPlaying == "true" {
-							reply = e.Nick + " está agora a ouvir " +
-								result.Tracks[0].Artist.Name + " - " + result.Tracks[0].Name
-						} else {
-							reply = e.Nick + " ouviu " +
-								result.Tracks[0].Artist.Name +
-								" - " + result.Tracks[0].Name
-						}
-					}
-				}
-			}
-			irccon.Privmsg(channel, reply)
-		}
-	})
-
-	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
-		req := strings.Split(e.Message(), " ")
-		if req[0] == "-lfmset" {
-			var reply string
-			if len(req) < 2 {
-				reply = "E o username?!"
-			} else {
-				_, err := db.Exec("insert into users(nick,lastfm) values('" + e.Nick + "','" + req[1] + "')")
-				if err != nil {
-					reply = "Não quero"
-				} else {
-					reply = "Tá guardado"
-				}
-			}
-			irccon.Privmsg(channel, reply)
-		}
-	})
-
 	irccon.AddCallback("JOIN", func(e *irc.Event) {
 		var reply string
 		if strings.EqualFold("Rick971", e.Nick) {
@@ -143,68 +89,134 @@ func main() {
 		irccon.Privmsg(channel, reply)
 	})
 
-	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
-		req := strings.Split(e.Message(), " ")
-		var user1 string
-		var user2 string
-		reply := "Não mandas em mim"
-		if len(req) > 2 {
-			rows, err := db.Query("select lastfm from users where nick = '" + req[1] + "'")
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Next()
-			rows.Scan(&user1)
-			rows, err = db.Query("select lastfm from users where nick = '" + req[2] + "'")
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Next()
-			rows.Scan(&user2)
-
-			result, err := api.Tasteometer.Compare(lastfm.P{"type1": "user",
-				"type2":  "user",
-				"value1": user1,
-				"value2": user2})
-			if err != nil {
-				reply = "Não quero"
-			}
-			percent, _ := strconv.ParseFloat(result.Result.Score, 64)
-			score := int(percent * 100)
-			reply = user1 + " tem " + strconv.Itoa(score) + "% de compatibilidade com " + user2 + "."
-		} else if len(req) > 1 {
-			rows, err := db.Query("select lastfm from users where nick = '" + e.Nick + "'")
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Next()
-			rows.Scan(&user1)
-			rows, err = db.Query("select lastfm from users where nick = '" + req[1] + "'")
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Next()
-			rows.Scan(&user2)
-			result, err := api.Tasteometer.Compare(lastfm.P{"type1": "user",
-				"type2":  "user",
-				"value1": user1,
-				"value2": user2})
-			if err != nil {
-				reply = "Não quero"
-			}
-			percent, _ := strconv.ParseFloat(result.Result.Score, 64)
-			score := int(percent * 100)
-			reply = user1 + " tem " + strconv.Itoa(score) + "% de compatibilidade com " + user2 + "."
-		} else {
-			reply = "Deslarga-me!"
-		}
-		irccon.Privmsg(channel, reply)
-	})
-
 	irccon.AddCallback("KICK", func(e *irc.Event) {
 		time.Sleep(1000 * time.Millisecond)
 		irccon.Join(channel)
 	})
 
+	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
+		req := strings.Split(e.Message(), " ")
+		if req[0] == "-np" {
+			nowPlaying(req, e)
+		} else if req[0] == "-lfmset" {
+			lfmSet(req, e)
+		} else if req[0] == "-compare" {
+			lfmCompare(req, e)
+		}
+	})
+
 	irccon.Loop()
+}
+
+func nowPlaying(req []string, e *irc.Event) {
+	var reply string
+	if len(req) > 1 {
+		result, err := api.User.GetRecentTracks(lastfm.P{"user": req[1]})
+		if err != nil {
+			reply = "Não quero"
+		} else if result.Tracks != nil {
+			if result.Tracks[0].NowPlaying == "true" {
+				reply = req[1] + " está agora a ouvir " + result.Tracks[0].Artist.Name + " - " + result.Tracks[0].Name
+			} else {
+				reply = req[1] + " ouviu " + result.Tracks[0].Artist.Name + " - " + result.Tracks[0].Name
+			}
+		}
+		reply = "Não quero fdp, deslarga-me crlh"
+	} else {
+		rows, err := db.Query("select lastfm from users where nick = '" + e.Nick + "'")
+		var lfm string
+		rows.Next()
+		rows.Scan(&lfm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if lfm == "" {
+			reply = "Não sei qual é o teu username fdp"
+		} else {
+			result, err := api.User.GetRecentTracks(lastfm.P{"user": lfm})
+			if err != nil {
+				reply = "Não quero"
+			} else {
+				if result.Tracks[0].NowPlaying == "true" {
+					reply = e.Nick + " está agora a ouvir " + result.Tracks[0].Artist.Name + " - " + result.Tracks[0].Name
+				} else {
+					reply = e.Nick + " ouviu " + result.Tracks[0].Artist.Name + " - " + result.Tracks[0].Name
+				}
+			}
+		}
+	}
+	irccon.Privmsg(channel, reply)
+}
+
+func lfmSet(req []string, e *irc.Event) {
+	var reply string
+	if len(req) < 2 {
+		reply = "E o username ó palhaço?!"
+	} else {
+		_, err := db.Exec("insert into users(nick,lastfm) values('" + e.Nick + "','" + req[1] + "')")
+		if err != nil {
+			reply = "Não quero"
+		} else {
+			reply = "Tá guardado"
+		}
+	}
+	irccon.Privmsg(channel, reply)
+}
+
+func lfmCompare(req []string, e *irc.Event) {
+	var user1 string
+	var user2 string
+	reply := "Não mandas em mim"
+	if len(req) > 2 {
+		rows, err := db.Query("select lastfm from users where nick = '" + req[1] + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Next()
+		rows.Scan(&user1)
+		rows, err = db.Query("select lastfm from users where nick = '" + req[2] + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Next()
+		rows.Scan(&user2)
+
+		result, err := api.Tasteometer.Compare(lastfm.P{"type1": "user",
+			"type2":  "user",
+			"value1": user1,
+			"value2": user2})
+		if err != nil {
+			reply = "Não quero"
+		}
+		percent, _ := strconv.ParseFloat(result.Result.Score, 64)
+		score := int(percent * 100)
+		reply = req[1] + " tem " + strconv.Itoa(score) + "% de compatibilidade com " + req[2] + "."
+	} else if len(req) > 1 {
+		rows, err := db.Query("select lastfm from users where nick = '" + e.Nick + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Next()
+		rows.Scan(&user1)
+		rows, err = db.Query("select lastfm from users where nick = '" + req[1] + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Next()
+		rows.Scan(&user2)
+		result, err := api.Tasteometer.Compare(lastfm.P{"type1": "user",
+			"type2":  "user",
+			"value1": user1,
+			"value2": user2})
+		if err != nil {
+			reply = "Não quero"
+		}
+		percent, _ := strconv.ParseFloat(result.Result.Score, 64)
+		score := int(percent * 100)
+		reply = e.Nick + " tem " + strconv.Itoa(score) + "% de compatibilidade com " + req[1] + "."
+	} else {
+		reply = "Deslarga-me!"
+	}
+	irccon.Privmsg(channel, reply)
+
 }
